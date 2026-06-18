f9ddd1cbb77d4d130a19d8d5c7beac39a2fe146b
Victor M. Peñaranda-Vélez <138157161+VictorPenaranda@users.noreply.github.com>
2026-06-18 13:20:26 -0600

    generator.m

---
+++ generator.m
@@ -0,0 +1,81 @@
+%---------------------------------------------------------------------------
+% 2D-Cascade Generator
+%---------------------------------------------------------------------------
+function [W] = generator(varargin)
+%---------------------------------------------------------------------------
+%---------------------------------------------------------------------------
+%
+% Syntax:
+%
+%    W = generator(codi,gammaless,gammaplus)
+%
+% Description:
+%
+%    This function is designed to estimate the multiplier statistics of the 
+%    random cascade model. To execute this function, the codimension of the 
+%    source precipitation field and the generator’s exponents, which represent 
+%    the increasing and decreasing transformations of the mass density, are 
+%    required.
+%
+% Input arguments:
+%
+%       codi:       Codimension of the source field.
+%       gammaless:  Negative gamma exponent.
+%       gammaplus:  Positive gamma exponent.
+%
+% Output arguments:
+%
+%       W:          Multipliers of the random cascade model.
+%
+% Last update: June 18, 2026
+% Authors:  Victor Penaranda-Velez (victor.penaranda[at]atmosfera.unam.mx)
+%           Carlos A. Ochoa-Moya (carlos.ochoa@atmosfera.unam.mx)
+%           Arturo I. Quintanar (arturo.quintanar@atmosfera.unam.mx)
+%---------------------------------------------------------------------------
+%---------------------------------------------------------------------------
+
+if nargin==3
+
+    % Variables
+    
+    codi = varargin{1};      % Codimension
+    
+    gammaless = varargin{2}; % Exponent of the cascade generator
+    
+    gammaplus = varargin{3}; % Exponent of the cascade generator
+
+    lambda0 = 0.5;           % Single-step scale ratio
+    
+    branch = 4;              % Branching number
+
+    W = zeros(1,branch);
+    
+    for ii = 1:2:length(W)
+    
+        rdn = rand(1);
+    
+        if rdn <= lambda0^codi
+    
+            W(ii) = lambda0^(gammaless);
+            
+            W(ii+1) = lambda0^(gammaplus);
+            
+        else
+    
+            W(ii) = lambda0^(gammaplus);
+            
+            W(ii+1) = lambda0^(gammaless);
+    
+        end
+        
+    end
+    
+    W = reshape(W,log2(branch),log2(branch));  % Sorting values in a matrix form
+    
+    W(:) = W(randperm(numel(W)));              % Multipliers
+
+else
+
+    error('Incorrect number of input variables')
+
+end
